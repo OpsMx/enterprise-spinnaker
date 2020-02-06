a066a9dc4a1c2f4d67598f6cc48aca6c6a00873d
sagayd <you@example.com>
2020-02-06 20:31:29 +0530

    Removing dirs

--- baremetalk8s-install/HAproxy/install_haproxy.sh
+++
@@ -1,5 +0,0 @@
-sudo add-apt-repository -y ppa:vbernat/haproxy-1.9
-sudo apt-get update
-sudo apt-get install -y haproxy
-sudo cp /vagrant/haproxy.cfg.sample /etc/haproxy/haproxy.cfg
-sudo service haproxy restart
