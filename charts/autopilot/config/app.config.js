1825da8e92061746464f51e4f479aeef37c4798f
Abhinay Byrisetty <abhinay.byrisetty@hotmail.com>
2020-06-29 20:15:01 +0530

    Update autopilot helm chart to allow more configurable values

--- charts/autopilot/config/app.config.js
+++ charts/autopilot/config/app.config.js
@@ -1,5 +1,5 @@
 var AppConfig = {
 	getBaseUrl:function() {
-		return "//" + window.location.hostname + ":{{ .Values.autopilot.backendPort }}";
+		return "//" + window.location.hostname + ":{{ .Values.autopilot.config.appConfigJS.backendPort }}";
 	}
 }
