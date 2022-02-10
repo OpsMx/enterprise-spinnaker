8ce735b9a1773cd5e2c143cef2d159fcbcc870e1
Ramya Ravi <72739416+ramyaravi-opsmx@users.noreply.github.com>
2022-02-10 17:06:41 +0530

    OP-12456: migration url input (#253)

--- scripts/oes-data-migration-scripts/migration_v3.9.x_to_v3.10.py
+++ scripts/oes-data-migration-scripts/migration_v3.9.x_to_v3.10.py
@@ -15,6 +15,8 @@ def perform_migration():
         print("Altered table approval_gate_approval in visibilitydb")
         modifyOpsmxdb()
         print("Altered column verification_type value of table userservicetemplate in opsmdb")
+        print("Alter autopilot db table entropy")
+        updatescript()
         modifyGit(hosturl)
         print("Modified config data of Datasource type 'GIT'")
         modifygitname()
@@ -41,11 +43,10 @@ def perform_migration():
         oesdb_conn.close()
 
 
-
 def perform_Audit_migration():
     try:
         print("Checking spinnaker configuration in oes db")
-        url = verifySpinnakerConfigurationAndGetURL()
+        verifySpinnakerConfigurationAndGetURL()
         print("Spinnaker configured url " + url)
         print("Fetch spinnaker applications")
         applications = fetchSpinnakerApplication(url)
@@ -64,14 +65,12 @@ def verifySpinnakerConfigurationAndGetURL():
         cur = oesdb_conn.cursor()
         cur.execute("select id,url from spinnaker;")
         result = cur.fetchall()
-        if result is not None:
-            url = result[0][1]
-        else:
+        if result is  None:
             raise Exception("Please configure spinnaker before proceeding with audit migration")
     except Exception as e:
         print("Exception occurred while fetching spinnaker configuration from oes db: ", e)
         raise e
-    return url;
+
 
 
 def fetchSpinnakerApplication(url):
@@ -96,7 +95,7 @@ def fetchSpinnakerPipelineExecutionByApp(applications, url):
             headers = {'Cookie': session_id}
             response = requests.get(url=updated_url, headers=headers)
             if response.json() != []:
-                #print("SPINNAKER APPLICATION - " + application + " PIPELINE DETAILS: " + str(response.json()))
+                # print("SPINNAKER APPLICATION - " + application + " PIPELINE DETAILS: " + str(response.json()))
                 pipelineArray = {application: json.loads(response.content)}
                 appcount += 1
                 print("Total application pipeline executions : " + str(appcount))
@@ -106,6 +105,7 @@ def fetchSpinnakerPipelineExecutionByApp(applications, url):
         print("Exception occurred while fetching spinnaker pipeline execution : ", e)
         raise e
     return applicationPipelineDict
+
 
 def fetchSpinnakerPipelineConfigExecution(applications, url):
     applicationPipelineConfigDict = []
@@ -117,7 +117,7 @@ def fetchSpinnakerPipelineConfigExecution(applications, url):
             headers = {'Cookie': session_id}
             response = requests.get(url=updated_url, headers=headers)
             if response.json() != []:
-                #print("SPINNAKER APPLICATION - " + application + " PIPELINE CONFIG DETAILS: " + str(response.json()))
+                # print("SPINNAKER APPLICATION - " + application + " PIPELINE CONFIG DETAILS: " + str(response.json()))
                 pipelineArray = {application: json.loads(response.content)}
                 appcount += 1
                 print("Total application pipeline config executions " + str(appcount))
@@ -127,6 +127,7 @@ def fetchSpinnakerPipelineConfigExecution(applications, url):
         print("Exception occurred while fetching spinnaker pipeline execution : ", e)
         raise e
     return applicationPipelineConfigDict
+
 
 def migratePipelineExecutions(applicationPipelineDict):
     try:
@@ -165,14 +166,15 @@ def migratePipelineExecutions(applicationPipelineDict):
                                     updated_pipeline_execution = json.loads(
                                         updated_pipeline_upper_json + pipelineExecutionJson + updated_pipeline_lower_json)
                                     updated_pipeline_execution_Json = json.dumps(updated_pipeline_execution)
-                                    #print("Updated Pipeline execution details for application : " + application + " :: " + updated_pipeline_execution_Json)
+                                    # print("Updated Pipeline execution details for application : " + application + " :: " + updated_pipeline_execution_Json)
                                     insertPipelineExecutionData(eventId, updated_pipeline_execution_Json)
                                     savedCount += 1
                                     print("********** Saved Pipeline Execution count::" + str(savedCount))
                     else:
                         rejectedCount += 1;
                         rejectedAppList.add(str(application))
-                        print("********** Received Rejected count of Pipeline Execution count::" + str(rejectedCount) + " application : " + str(application))
+                        print("********** Received Rejected count of Pipeline Execution count::" + str(
+                            rejectedCount) + " application : " + str(application))
                         rejectedPipelineExecutionJson.append(str(pipelineExecution));
         print("Total Received count : " + str(startcount))
         print("Total Saved count : " + str(savedCount))
@@ -181,10 +183,12 @@ def migratePipelineExecutions(applicationPipelineDict):
         print("Exception occurred while updating pipeline execution data : ", e)
         raise e
 
+
 def getEventId(pipelineExecution):
     if 'eventId' in pipelineExecution['trigger']:
         eventId = pipelineExecution['trigger']['eventId']
-    elif ('parentExecution' in pipelineExecution['trigger'] and 'trigger' in  pipelineExecution['trigger']['parentExecution'] and 'eventId' in pipelineExecution['trigger']['parentExecution']['trigger']):
+    elif ('parentExecution' in pipelineExecution['trigger'] and 'trigger' in pipelineExecution['trigger'][
+        'parentExecution'] and 'eventId' in pipelineExecution['trigger']['parentExecution']['trigger']):
         eventId = pipelineExecution['trigger']['parentExecution']['trigger']['eventId']
     else:
         eventId = str(uuid.uuid4())
@@ -232,11 +236,12 @@ def isDataAlreadyPresent(application, executionId):
         print("Exception occurred while fetch data into audit_events table : ", e)
         raise e
 
+
 def migratePipelineConfigExecutionS(applicationPipelineDict):
     try:
         startcount = 0;
         savedCount = 0;
-        rejectedCount=0;
+        rejectedCount = 0;
         rejectedPipelineExecutionJson = []
         rejectedAppList = []
         for applicationPipelines in applicationPipelineDict:
@@ -251,32 +256,40 @@ def migratePipelineConfigExecutionS(applicationPipelineDict):
                         pipelineId = pipelineExecutionConfig['id']
                         pipelineName = pipelineExecutionConfig['name']
                         updatedTime = pipelineExecutionConfig['updateTs']
-                        print("** Extracted pipeline config Data  user: " + user + ",application: " + application + ",pipelineId: " + pipelineId + ",pipelineName: " + pipelineName)
-                        updated_pipeline_config_upper_json =  json.loads(pipeline_config_upper_json.replace('{user}',user).replace('{appName}', application).replace('{pipelineId}', pipelineId).replace('{pipelineName}', pipelineName))
+                        print(
+                            "** Extracted pipeline config Data  user: " + user + ",application: " + application + ",pipelineId: " + pipelineId + ",pipelineName: " + pipelineName)
+                        updated_pipeline_config_upper_json = json.loads(
+                            pipeline_config_upper_json.replace('{user}', user).replace('{appName}',
+                                                                                       application).replace(
+                                '{pipelineId}', pipelineId).replace('{pipelineName}', pipelineName))
                         updated_pipeline_config_execution_Json = json.dumps(updated_pipeline_config_upper_json)
-                        print("Updated Pipeline config execution details for application : " + application + " :: " + updated_pipeline_config_execution_Json)
+                        print(
+                            "Updated Pipeline config execution details for application : " + application + " :: " + updated_pipeline_config_execution_Json)
                         eventId = uuid.uuid4()
-                        insertPipelineConfigExecutionData(updatedTime,eventId, updated_pipeline_config_execution_Json)
+                        insertPipelineConfigExecutionData(updatedTime, eventId, updated_pipeline_config_execution_Json)
                         savedCount += 1
                         print("********** Saved Pipeline Execution count::" + str(savedCount))
                     else:
                         rejectedCount += 1;
                         rejectedAppList.append(str(application))
-                        print("********** Received Rejected count of Pipeline config Execution count::" + str(rejectedCount) + " application : "+str(application))
+                        print("********** Received Rejected count of Pipeline config Execution count::" + str(
+                            rejectedCount) + " application : " + str(application))
                         rejectedPipelineExecutionJson.append(str(pipelineExecutionConfig));
         print("Total Received count : " + str(startcount))
-        print("Total Saved count : "+str(savedCount))
-        print("Total Rejected count : "+str(rejectedCount)+" Rejected App list :"+str(rejectedAppList))
+        print("Total Saved count : " + str(savedCount))
+        print("Total Rejected count : " + str(rejectedCount) + " Rejected App list :" + str(rejectedAppList))
     except Exception as e:
         print("Exception occurred while updating pipeline execution data : ", e)
         raise e
 
-def insertPipelineConfigExecutionData(updatedTime,eventId, updated_pipeline_config_execution):
+
+def insertPipelineConfigExecutionData(updatedTime, eventId, updated_pipeline_config_execution):
     try:
         cur = audit_conn.cursor()
-        #date_time = datetime.datetime.now()
-        updatedTime_date_time = datetime.datetime.utcfromtimestamp(int(updatedTime)/1000)
-        data = str(updatedTime_date_time), str(updatedTime_date_time), updated_pipeline_config_execution, str(eventId), 'spinnaker '
+        # date_time = datetime.datetime.now()
+        updatedTime_date_time = datetime.datetime.utcfromtimestamp(int(updatedTime) / 1000)
+        data = str(updatedTime_date_time), str(updatedTime_date_time), updated_pipeline_config_execution, str(
+            eventId), 'spinnaker '
         cur.execute(
             "INSERT INTO audit_events (created_at, updated_at,data,event_id,source) VALUES (%s, %s, %s, %s, %s)",
             data)
@@ -285,12 +298,13 @@ def insertPipelineConfigExecutionData(updatedTime,eventId, updated_pipeline_conf
         print("Exception occurred while inserting  pipeline config data into audit_events table : ", e)
         raise e
 
+
 def getGitUsernameBytoken(token, url):
     try:
-        url = url+"/user"
+        url = url + "/user"
         headers = {'Authorization': 'token ' + token}
         login = requests.get(url=url, headers=headers).json()
-        print("git username: "+login['login'])
+        print("git username: " + login['login'])
         return login['login']
     except Exception as e:
         print("Exception occured while getting user name of datasource type GIT : ", e)
@@ -320,6 +334,16 @@ def modifyOpsmxdb():
         raise e
 
 
+def updatescript():
+    try:
+        cur = opsmxdb_conn.cursor()
+        cur.execute(" ALTER TABLE entropy ALTER COLUMN service_id DROP NOT NULL ")
+        print("Successfully altered entropy table in autopilot db")
+    except Exception as e:
+        print("Exception occured while  updating script : ", e)
+        raise e
+
+
 def modifyGit(hosturl):
     try:
         cur = platform_conn.cursor()
@@ -328,13 +352,16 @@ def modifyGit(hosturl):
         if result != None:
             for data in result:
                 configData = json.loads(data[1])
-                jdata = {"hostUrl":hosturl,"url":configData['url'],"username":getGitUsernameBytoken(configData['token'],configData['url']),"token":configData['token']}
-                updatedConfig = "'"+str(json.dumps(jdata))+"'"
-                print("GIT Datasource Json data of Id:"+str(data[0])+" :"+updatedConfig)
-                cur.execute('update datasource SET config ='+updatedConfig+' where id ='+ str(data[0]))
+                jdata = {"hostUrl": hosturl, "url": configData['url'],
+                         "username": getGitUsernameBytoken(configData['token'], configData['url']),
+                         "token": configData['token']}
+                updatedConfig = "'" + str(json.dumps(jdata)) + "'"
+                print("GIT Datasource Json data of Id:" + str(data[0]) + " :" + updatedConfig)
+                cur.execute('update datasource SET config =' + updatedConfig + ' where id =' + str(data[0]))
     except Exception as e:
         print("Exception occurred while modify datasource data of GIT: ", e.with_traceback())
         raise e
+
 
 def modifygitname():
     try:
@@ -349,7 +376,7 @@ def modifygitname():
         raise e
 
 
-def modifyGithub(hosturl,url):
+def modifyGithub(hosturl, url):
     try:
         cur = platform_conn.cursor()
         cur.execute("select id,config from datasource where datasourcetype = 'GITHUB';")
@@ -360,10 +387,10 @@ def modifyGithub(hosturl,url):
                 updateUsername = " "
                 if 'username' in configData:
                     updateUsername = configData['username']
-                jdata = {"hostUrl":hosturl,"url":url,"username":updateUsername,"token":configData['token']}
-                updatedConfig = "'"+str(json.dumps(jdata))+"'"
-                print("GITHUB Datasource Json data of Id: "+str(data[0])+" :"+updatedConfig)
-                cur.execute('update datasource SET config ='+updatedConfig+' where id=' +str(data[0]))
+                jdata = {"hostUrl": hosturl, "url": url, "username": updateUsername, "token": configData['token']}
+                updatedConfig = "'" + str(json.dumps(jdata)) + "'"
+                print("GITHUB Datasource Json data of Id: " + str(data[0]) + " :" + updatedConfig)
+                cur.execute('update datasource SET config =' + updatedConfig + ' where id=' + str(data[0]))
     except Exception as e:
         print("Exception occurred while modify datasource of tpe GITHUB: ", e)
         raise e
@@ -395,9 +422,11 @@ def migrate_policy_audit(policy_audits):
                 "auditData": audit
             }
             opsmxtime = str(policy_audit[10])
-            print("Policy data inserting into DB: "+str(audit_data))
+            print("Policy data inserting into DB: " + str(audit_data))
             data = opsmxtime, opsmxtime, json.dumps(audit_data), audit_data['eventId'], 'OES'
-            cur.execute("INSERT INTO audit_events (created_at, updated_at, data, event_id, source) VALUES (%s, %s, %s, %s, %s)", data)
+            cur.execute(
+                "INSERT INTO audit_events (created_at, updated_at, data, event_id, source) VALUES (%s, %s, %s, %s, %s)",
+                data)
 
     except Exception as e:
         print("Exception occurred while migrating policy audit : ", e)
@@ -407,7 +436,8 @@ def migrate_policy_audit(policy_audits):
 def fetch_policy_audit():
     try:
         cur = oesdb_conn.cursor()
-        cur.execute("select action, application, description, execution_id, gate, pipeline, policy_event, policy_name, result, user_id,created_date from policy_audit")
+        cur.execute(
+            "select action, application, description, execution_id, gate, pipeline, policy_event, policy_name, result, user_id,created_date from policy_audit")
         return cur.fetchall()
     except Exception as e:
         print("Exception occurred while fetching policy audit : ", e)
@@ -416,9 +446,10 @@ def fetch_policy_audit():
 
 if __name__ == '__main__':
     n = len(sys.argv)
-    if n != 12:
-        print('Please pass valid 11 arguments visibilitydb <visibility-db-host> <platform_db-name> <platform_hostt> <opsmx-db-name> <opsmx-db-host> '
-              '<oes-db-name> <oes-db-host> <audit_db-name> <audit_db_host> <db-port> <session_id>(configured spinnaker active session Id)')
+    if n != 13:
+        print(
+            'Please pass valid 11 arguments visibilitydb <visibility-db-host> <platform_db-name> <platform_hostt> <opsmx-db-name> <opsmx-db-host> '
+            '<oes-db-name> <oes-db-host> <audit_db-name> <audit_db_host> <db-port> <url>(spinnaker gate url) <session_id>(configured spinnaker active session Id)')
 
     visibility_db = 'visibilitydb'
     visibility_host = sys.argv[2]
@@ -431,8 +462,8 @@ if __name__ == '__main__':
     audit_db = sys.argv[9]
     audit_host = sys.argv[10]
     port = sys.argv[11]
-    session_id = sys.argv[12]
-
+    url = sys.argv[12]
+    session_id = sys.argv[13]
 
     print("Using default host url ex:http://github.com")
 
