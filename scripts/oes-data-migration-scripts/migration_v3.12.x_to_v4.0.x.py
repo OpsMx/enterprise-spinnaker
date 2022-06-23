6b3df76e752a479f528ebb052a915e861d97de40
Ramya Ravi <72739416+ramyaravi-opsmx@users.noreply.github.com>
2022-06-23 12:49:15 +0530

    OP-16310- migration script v3.12.5 to v3.12.6 (#330)

--- scripts/oes-data-migration-scripts/migration_v3.12.x_to_v4.0.x.py
+++ scripts/oes-data-migration-scripts/migration_v3.12.x_to_v4.0.x.py
@@ -17,6 +17,9 @@ def perform_migration():
         update_autopilot_constraints()
         autopilot_conn.commit()
         print("successfully migrated autopilot db")
+        updateApprovalGateAUdit()
+        audit_conn.commit()
+        print("successfully migrated audit db")
 
     except Exception as e:
         print("Exception occurred while migration : ", e)
@@ -102,14 +105,62 @@ def updateRefId():
         print("Exception occured in updateRefId while updating script : ", e)
         raise e
 
+def updateApprovalGateAUdit():
+    try:
+        cur_audit.execute("select id,data from audit_events where data->>'eventType' = 'APPROVAL_GATE_AUDIT'")
+        approval_audit_details = cur_audit.fetchall()
+        for auditdata in approval_audit_details:
+            jsonData = json.loads(json.dumps(listData(auditdata)))
+            if 'gateId' in jsonData['auditData']['details']:
+                audit_events_table_id = auditdata[0]
+                gateId = jsonData['auditData']['details']['gateId']
+                cur_visibility.execute('select pipeline_id from approval_gate where id =' + str(gateId))
+                pipelineId = cur_visibility.fetchone()
+                if pipelineId is not None:
+                  pipelineId = str(pipelineId[0])
+                  cur_platform.execute('select distinct(a.name) as applicationName from applications a left outer join service s on a.id=s.application_id left outer join service_pipeline_map sp on s.id=sp.service_id where sp.pipeline_id =' + str(pipelineId))
+                  applicationName = cur_platform.fetchone()
+                  if applicationName is not None:
+                      applicationName = str(applicationName[0])
+                      print("GateId: {},pipelineId: {} ,applicationName: {} ,audit_events_table_id :{}".format(gateId,pipelineId,applicationName,audit_events_table_id))
+                      fetchJsonAndUpdate(audit_events_table_id,applicationName,jsonData)
+    except Exception as e:
+      print("Exception occurred while  updating script : ", e)
+      raise e
+
+def listData(results):
+    resultData = {}
+    for result in results :
+            resultData = result
+    return resultData
+
+
+
+def fetchJsonAndUpdate(audit_events_table_id, applicationName,jsonData):
+    try:
+        oldAppName = jsonData['auditData']['details']['application']
+        if oldAppName is not applicationName:
+            updateJson = json.loads(json.dumps(jsonData).replace(oldAppName,applicationName))
+            updateApprovalAuditJson(audit_events_table_id,updateJson)
+    except Exception as e:
+        print("Exception occurred while mapping application name : ", e)
+        raise e
+
+def  updateApprovalAuditJson(audit_events_table_id,updateJson):
+    try:
+        updatedConfig = "'" + str(json.dumps(updateJson)) + "'"
+        cur_audit.execute('update audit_events set data ='+updatedConfig+' where id ={}'.format(audit_events_table_id))
+    except Exception as e:
+        print("Exception occurred while updating update json : ", e)
+        raise e
 
 
 if __name__ == '__main__':
     n = len(sys.argv)
 
-    if n != 8:
+    if n != 12:
         print(
-            "Please pass valid 7 arguments <platform_db-name> <platform_host> <oes-db-name> <oes-db-host> <db-port> <user-name> <password>")
+            "Please pass valid 11 arguments <platform_db-name> <platform_host> <oes-db-name> <oes-db-host> <audit_db-name> <audit-db-host> <visibility_db-name> <visibility-db-host> <db-port> <user-name> <password>")
 
     platform_db = sys.argv[1]
     platform_host = sys.argv[2]
@@ -117,9 +168,13 @@ if __name__ == '__main__':
     oes_host = sys.argv[4]
     autopilot_db = sys.argv[5]
     autopilot_host = sys.argv[6]
-    port = sys.argv[7]
-    user_name = sys.argv[8]
-    password = sys.argv[9]
+    audit_db = sys.argv[7]
+    audit_host = sys.argv[8]
+    visibility_db = sys.argv[9]
+    visibility_host = sys.argv[10]
+    port = sys.argv[11]
+    user_name = sys.argv[12]
+    password = sys.argv[13]
 
     # Establishing the platform db connection
     platform_conn = psycopg2.connect(database=platform_db, user=user_name, password=password, host=platform_host, port=port)
@@ -134,7 +189,19 @@ if __name__ == '__main__':
     autopilot_conn = psycopg2.connect(database=autopilot_db, user=user_name, password=password, host=autopilot_host
                                       , port=port)
     print("autopilot database connection established successfully")
+
+    # Establishing the audit db connection
+    audit_conn = psycopg2.connect(database=audit_db, user=user_name, password=password, host=audit_host, port=port)
+    print('Opened audit database connection successfully')
+
+    # Establishing the visibility db connection
+    visibility_conn = psycopg2.connect(database=visibility_db, user=user_name, password=password, host=visibility_host,
+                                       port=port)
+    print("Visibility database connection established successfully")
+
     cur_platform = platform_conn.cursor()
     cur_oesdb = oesdb_conn.cursor()
     cur_autopilot = autopilot_conn.cursor()
+    cur_audit = audit_conn.cursor()
+    cur_visibility = visibility_conn.cursor()
     perform_migration()
