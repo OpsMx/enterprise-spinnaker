2831d57acbd6cb87f84cea0e2fcf4a4aa517b71a
Ravikumar Hawaldar <ravikumar.hawaldar@opsmx.io>
2022-07-26 20:31:08 +0530

    OP-16990 : Refactored the logic of deleting the records

--- scripts/oes-data-migration-scripts/clean_unused_audit_data.py
+++ scripts/oes-data-migration-scripts/clean_unused_audit_data.py
@@ -4,63 +4,18 @@ import psycopg2
 
 
 def delete_unused_audit_data():
-    audit_ids_to_be_retained = []
-    step = audit_cursor.itersize
-
-    print('Fetching audit data started.')
-    logging.info('Fetching audit data started.')
     try:
-        audit_cursor.execute('select id, data from audit_events;')
-        i = 0
+        print('Removing unused audit data query started.')
+        logging.info('Removing unused audit data query started.')
 
-        for row in audit_cursor:
-            audit_data_dict = row[1]
-            i += 1
+        audit_cursor.execute("""DELETE FROM audit_events WHERE id NOT IN (select id from audit_events 
+        where data -> 'details' ->> 'type' IN ('orca:pipeline:complete', 'orca:pipeline:failed', 'orca:stage:starting', 
+        'orca:stage:complete', 'orca:stage:failed', 'orca:pipeline:starting', 'orca:orchestration:starting', 
+        'orca:orchestration:complete')) and "source" = 'spinnaker' RETURNING id;""")
 
-            if 'eventType' in audit_data_dict:
-                audit_ids_to_be_retained.append(row[0])
-                continue
-            if 'details' in audit_data_dict:
-                details_dict = audit_data_dict['details']
-                if 'type' in details_dict:
-                    details_type = details_dict['type']
-                    if details_type in used_pipeline_execution_events:
-                        audit_ids_to_be_retained.append(row[0])
-                        continue
-
-            if 'content' in audit_data_dict:
-                content_dict = audit_data_dict['content']
-                if 'execution' in content_dict:
-                    execution_dict = content_dict['execution']
-                    if 'stages' in execution_dict:
-                        stages_list = execution_dict['stages']
-                        for stages_dict in stages_list:
-                            if 'type' in stages_dict:
-                                stage_type = stages_dict['type']
-                                if stage_type in app_onboarding_events:
-                                    audit_ids_to_be_retained.append(row[0])
-                                    break
-
-            if i % step == 0:
-                print('Number of rows Processed ', str(i))
-                logging.info('Number of rows Processed ' + str(i))
-
-        print('Successfully fetched and identified the unused rows for deletion.')
-        logging.info('Successfully fetched and identified the unused rows for deletion.')
-
-        logging.info('Audit events ids which are to be retained ' + '\n' + ''.join(str(audit_ids_to_be_retained)))
-
-        print('Removing unused audit data started.')
-        logging.info('Removing unused audit data started.')
-
-        audit_del_cursor = audit_conn.cursor()
-        query = 'DELETE FROM audit_events WHERE NOT (id = ANY(%s)) RETURNING id;'
-        audit_del_cursor.execute(query, (audit_ids_to_be_retained,))
-        result = audit_del_cursor.fetchall()
+        result = audit_cursor.fetchall()
 
         logging.info('Audit event ids which got deleted ' + '\n' + ''.join(str(result)))
-
-        logging.info('The number of ids that are to be retained is ' + str(len(audit_ids_to_be_retained)))
         logging.info('The number of ids that got deleted is ' + str(len(result)))
 
         audit_conn.commit()
@@ -106,7 +61,6 @@ if __name__ == '__main__':
     app_onboarding_events = ['createApplication', 'updateApplication', 'deleteApplication', 'savePipeline',
                              'updatePipeline', 'deletePipeline']
 
-    audit_cursor = audit_conn.cursor('audit_cur')
-    audit_cursor.itersize = 5000
+    audit_cursor = audit_conn.cursor()
 
     delete_unused_audit_data()
