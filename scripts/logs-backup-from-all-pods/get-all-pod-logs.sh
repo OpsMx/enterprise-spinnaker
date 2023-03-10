e117e698bb82727d4b886939b682c1c5d6fb59d8
Rahul Chekuri <89373036+rahul-chekuri@users.noreply.github.com>
2023-03-10 19:48:25 +0530

    OP-18601: script changes to fetch logs/counts. (#459)
    * OP-18601: script changes to fetch logs/counts.
    
    * OP-18601: python3
    
    * OP-18601: Connecting audit db to fetch active users count.
    
    * OP-18601: Connecting audit db to fetch active users count.
    
    * OP-18601: uncommented the code fragments.
    
    * OP-18601: close connections.
    
    * OP-18601: removed header

--- scripts/logs-backup-from-all-pods/get-all-pod-logs.sh
+++ scripts/logs-backup-from-all-pods/get-all-pod-logs.sh
@@ -2,6 +2,19 @@
 # and tar and gzip those files
 #!/bin/bash
 ns=$1
+ISD_ADMIN_USERNAME=$2
+ISD_ADMIN_PASSWORD=$3
+REDIS_HOST=$4
+REDIS_PORT=$5
+REDIS_PASSWORD=$6
+ISD_GATE_URL=$7
+DAYS=$8
+INSTALLATION_TYPE=$9
+PLATFORM_DB=${10}
+PLATFORM_HOST=${11}
+USER_NAME=${12}
+PASSWORD=${13}
+PORT=${14}
 [ -z "$ns" ] && echo "Please pass namespace as an argument"
 [ -z "$ns" ] && exit 1
 FILE=logs.tar            
@@ -14,6 +27,7 @@ echo getting logs from $ns
 cd /tmp
 rm -rf logdir
 rm -rf logs*.tar.gz
+rm -rf logs*.zip
 mkdir -p logdir
 kubectl get po -n $ns --no-headers > logdir/pods.txt
 while read -r line;
@@ -23,7 +37,7 @@ do
   echo getting logs for $podname
   kubectl -n $ns logs $podname -c $containername > logdir/"$podname".log
 done < logdir/pods.txt
-tar -cvf $filename logdir
-gzip $filename
+python3 extract_logs.py $ISD_ADMIN_USERNAME $ISD_ADMIN_PASSWORD $REDIS_HOST $REDIS_PORT $REDIS_PASSWORD $ISD_GATE_URL $DAYS $INSTALLATION_TYPE $PLATFORM_DB $PLATFORM_HOST $USER_NAME $PASSWORD $PORT
+zip -r -e "$filename.zip" logdir -P 'opsmx-password'
 rm -rf logdir
-echo please send /tmp/$filename.gz to opsmx by email
+echo please send /tmp/$filename.zip to opsmx by email
